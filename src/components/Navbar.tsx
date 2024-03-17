f8aa4d7b30aa503ff0723af17933b6c85b66bb9b
samuelsenerwa <samuelsenerwa@gmail.com>
2024-03-17 16:58:49 +0300

    feat:Added mobile navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Menu from './Menu'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase'>
+        {/* LOGO */}
+        <div className='text-xl'>
+            <Link href="/">Order Bite</Link>
+        </div>
+        {/* MOBILE MENU */}
+        <div>
+            <Menu />
+        </div>
+
+    </div>
   )
 }
 
--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Notification = () => {
   return (
-    <div className='h-12 bg-red-500 text-white px-4 py-4 flex items-center justify-center text-center text-sm'>Free delivery for all orders over $45. Order your food now!</div>
+    <div className='h-12 bg-red-500 text-white px-4 py-4 flex items-center justify-center text-center text-base cursor-pointer'>Free delivery for all orders over $45. Order your food now!</div>
   )
 }
 
