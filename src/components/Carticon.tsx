0442268f09396c90faa9ed4b831005716a76f5b5
samuelsenerwa <samuelsenerwa@gmail.com>
2024-03-17 17:21:07 +0300

    feat: added main navbar component

--- src/components/Carticon.tsx
+++ src/components/Carticon.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Carticon = () => {
   return (
     <Link href="/cart" className="flex items-center gap-4">
-      <div className="relative h-8 w-8">
+      <div className="relative h-8 w-8 md:w-5 md:h-5">
         <Image src="/cart.png" alt="" fill />
       </div>
       <span>Cart (3)</span>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,38 @@
-import React, { useState } from 'react'
-import Menu from './Menu'
-import Link from 'next/link'
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Link from "next/link";
+import Carticon from "./Carticon";
+import Image from "next/image";
 
 const Navbar = () => {
+  const user = false;
   return (
-    <div className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase'>
-        {/* LOGO */}
-        <div className='text-xl'>
-            <Link href="/">Order Bite</Link>
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
+      {/* LEFT LINKS */}
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link href="/">Home</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
+      {/* LOGO */}
+      <div className="text-xl md:font-bold flex-1 md:text-center">
+        <Link href="/">Order Bite</Link>
+      </div>
+      {/* MOBILE MENU */}
+      <div>
+        <Menu />
+      </div>
+      {/* RIGHT LINKS */}
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className="md:absolute top-3 r-2 lg:static flex item-center gap-2 cursor-pointer bg-orange-300 rounded-md">
+          <Image src="/phone.png" alt="" width={20} height={20} />
+          <span>+1 2344 2311</span>
         </div>
-        {/* MOBILE MENU */}
-        <div>
-            <Menu />
-        </div>
-
+        {!user ? <Link href="/">Login</Link> : <Link href="/menu">Orders</Link>}
+        <Carticon />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
