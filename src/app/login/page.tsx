ca9237909b2eb6f604e242c59ea802027cc23117
samuelsenerwa <samuelsenerwa@gmail.com>
2024-03-19 15:55:13 +0300

    feat: performed google authentication

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+  const { data, status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <p>Loading ...</p>;
+  }
+
+  if (status === "authenticated") {
+    router.push("/");
+  }
+
+  console.log("data" + data);
+  console.log("status" + status);
   return (
     <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center ">
       {/* BOX */}
@@ -15,7 +31,10 @@ const LoginPage = () => {
         <div className="p-10 flex flex-col gap-8 md:w-1/2">
           <h1 className="font-bold text-xl xl:text-3xl">Welcome</h1>
           <p>Login your account or create a new one using social button</p>
-          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
+          <button
+            className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md"
+            onClick={() => signIn("google")}
+          >
             <Image
               src="/google.png"
               alt=""
