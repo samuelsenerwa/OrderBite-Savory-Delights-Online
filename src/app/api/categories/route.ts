7fefd4ccc860d6195cf90ebe48ac8a01445ffb02
samuelsenerwa <samuelsenerwa@gmail.com>
2024-03-19 10:53:41 +0300

    feat: created prisma && docker file for postgres

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient(); //initializing prisma
+
+// Fetch all the categories from prisma
+
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return new NextResponse(JSON.stringify(categories), {
+      status: 200,
+    });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
+
+export const POST = () => {
+  return new NextResponse("hello", { status: 200 });
+};
