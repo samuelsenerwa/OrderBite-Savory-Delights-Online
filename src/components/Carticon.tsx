b6f5dad2a13d47fcf9a71e4a495ab27b13d55ed6
samuelsenerwa <samuelsenerwa@gmail.com>
2024-03-20 21:35:31 +0300

    feat: update the count of items added to cart

--- src/components/Carticon.tsx
+++ src/components/Carticon.tsx
@@ -1,14 +1,18 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import { useCartStore } from "@/utils/store";
 
 const Carticon = () => {
+  const { totalItems } = useCartStore();
   return (
     <Link href="/cart" className="flex items-center gap-4">
       <div className="relative h-8 w-8 md:w-5 md:h-5">
         <Image src="/cart.png" alt="" fill />
       </div>
-      <span>Cart (3)</span>
+      <span>Cart ({totalItems})</span>
     </Link>
   );
 };
