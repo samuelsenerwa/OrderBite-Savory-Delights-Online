386347ca9f9ecd3801ef9e3e3f76a4282a67e5b7
samuelsenerwa <samuelsenerwa@gmail.com>
2024-03-21 01:55:32 +0300

    feat: handled delete order

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/utils/connect";
+import { getAuthSession } from "@/utils/auth";
 
 // GET single Product
 
@@ -21,4 +22,35 @@ export const GET = async (
       { status: 500 }
     );
   }
+};
+
+// DELETE single Product
+
+export const DELETE = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const { id } = params;
+  const session = await getAuthSession();
+
+  if (session?.user.isAdmin) {
+    try {
+      await prisma.product.delete({
+        where: { id: id },
+      });
+      return new NextResponse(JSON.stringify("Product has been deleted!"), {
+        status: 200,
+      });
+    } catch (err) {
+      console.log(err);
+      return new NextResponse(
+        JSON.stringify({ message: "You're not allowed" }),
+        { status: 500 }
+      );
+    }
+  }
+  return new NextResponse(
+    JSON.stringify({ message: "Something went wrong!" }),
+    { status: 403 }
+  );
 };
