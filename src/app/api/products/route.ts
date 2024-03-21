f86ad1049fb1399fa601703a77a3b743b9525fe2
samuelsenerwa <samuelsenerwa@gmail.com>
2024-03-21 17:31:38 +0300

    feat: completed uploading of items by admin

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -24,6 +24,19 @@ export const GET = async (req: NextRequest) => {
   }
 };
 
-export const POST = () => {
-  return new NextResponse("hello", { status: 200 });
+// send form inputs under add product page on admin's page
+export const POST = async (req: NextRequest) => {
+  try {
+    const body = await req.json();
+    const product = await prisma.product.create({
+      data: body,
+    });
+    return new NextResponse(JSON.stringify(product), { status: 201 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import Menu from "./Menu";
 import Link from "next/link";
-import Carticon from "./Carticon";
-import UserLinks from "./UserLinks";
+import CartIcon from "./Carticon";
 import Image from "next/image";
+import UserLinks from "./UserLinks";
 
 const Navbar = () => {
   const user = false;
@@ -11,13 +11,13 @@ const Navbar = () => {
     <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
       {/* LEFT LINKS */}
       <div className="hidden md:flex gap-4 flex-1">
-        <Link href="/">Home</Link>
+        <Link href="/">Homepage</Link>
         <Link href="/menu">Menu</Link>
         <Link href="/">Contact</Link>
       </div>
       {/* LOGO */}
       <div className="text-xl md:font-bold flex-1 md:text-center">
-        <Link href="/">Order Bite</Link>
+        <Link href="/">Massimo</Link>
       </div>
       {/* MOBILE MENU */}
       <div className="md:hidden">
@@ -25,12 +25,12 @@ const Navbar = () => {
       </div>
       {/* RIGHT LINKS */}
       <div className="hidden md:flex gap-4 items-center justify-end flex-1">
-        <div className="md:absolute top-3 r-2 lg:static flex item-center gap-2 cursor-pointer bg-orange-300 rounded-md">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
           <Image src="/phone.png" alt="" width={20} height={20} />
-          <span>+1 2344 2311</span>
+          <span>123 456 78</span>
         </div>
         <UserLinks />
-        <Carticon />
+        <CartIcon />
       </div>
     </div>
   );
